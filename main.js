42292f352f7fb97106941a780981549712d481d6
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-08-20 14:59:24 +0530

    Add files via upload
    Project 111

---
+++ main.js
@@ -0,0 +1,35 @@
+var prediction_1 = "";
+var prediction_2 = "";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function takeSnapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured_img' src='" + data_uri + "'/>";
+        console.log("captured image");
+    });
+}
+
+console.log(ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/agX16mAlg/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is" + prediction_2;
+    utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
