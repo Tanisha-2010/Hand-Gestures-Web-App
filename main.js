016c54f87c3f4d3d1a55e8031f95d323d4a4ad49
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-08-22 20:07:15 +0530

    Completed !!!!

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function takeSnapshot() {
 
 console.log(ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/agX16mAlg/model.json", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/VmufY7MCA/model.json", modelLoaded);
 
 function modelLoaded() {
     console.log("Model is Loaded");
@@ -32,4 +32,71 @@ function speak() {
     speak_data_2 = "And the second prediction is" + prediction_2;
     utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
+}
+
+function check() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img, getResults);
+}
+
+function getResults(error, result) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+        document.getElementById("prediction1").innerHTML = result[0].label;
+        document.getElementById("prediction2").innerHTML = result[1].label;
+
+        prediction_1 = result[0].label;
+        prediction_2 = result[1].label;
+        speak();
+
+        if (result[0].label == "Okay") {
+            document.getElementById("emoji_img").innerHTML = "&#128076;";
+        }
+
+        if (result[0].label == "Super") {
+            document.getElementById("emoji_img").innerHTML = "&#128077;";
+        }
+
+        if (result[0].label == "Victory") {
+            document.getElementById("emoji_img").innerHTML = "&#9996;";
+        }
+
+        if (result[0].label == "Raised Fist") {
+            document.getElementById("emoji_img").innerHTML = "&#9994;";
+        }
+
+        if (result[0].label == "Clap") {
+            document.getElementById("emoji_img").innerHTML = "&#128079;";
+        }
+
+        if (result[0].label == "Rock On") {
+            document.getElementById("emoji_img").innerHTML = "&#129304;";
+        }
+
+        if (result[1].label == "Okay") {
+            document.getElementById("emoji_img2").innerHTML = "&#128076;";
+        }
+
+        if (result[1].label == "Super") {
+            document.getElementById("emoji_img2").innerHTML = "&#128077;";
+        }
+
+        if (result[1].label == "Victory") {
+            document.getElementById("emoji_img2").innerHTML = "&#9996;";
+        }
+
+        if (result[1].label == "Raised Fist") {
+            document.getElementById("emoji_img2").innerHTML = "&#9994;";
+        }
+
+        if (result[1].label == "Clap") {
+            document.getElementById("emoji_img2").innerHTML = "&#128079;";
+        }
+
+        if (result[1].label == "Rock On") {
+            document.getElementById("emoji_img2").innerHTML = "&#129304;";
+        }
+    }
 }
